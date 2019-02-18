521e2be7c89930d504ba58f8f1a26ae870f14e4a
lbatonnet.cdi <lbatonnet.cdi@PC13-ECOLE>
2019-02-18 09:45:12 +0400

    modif annonce

---
+++ PlateformeAnnonceBackend/Models/Annonce.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlateformeAnnonceBackend.Models
+{
+    public class Annonce
+    {
+        public int Id { get; set; }
+        public string Titre { get; set; }
+        public string Date { get; set; }
+        public string Contenu { get; set; }
+        public string Prix { get; set; }
+        public string UrlPhoto { get; set; }
+    }
+}
