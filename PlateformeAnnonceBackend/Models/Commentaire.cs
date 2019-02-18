f3216d9d43c52c3fcc53f1a50c4ffddfb6880446
Richard LEGROS <174721@supinfo.com>
2019-02-18 10:04:51 +0400

    models

---
+++ PlateformeAnnonceBackend/Models/Commentaire.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlateformeAnnonceBackend.Models
+{
+    public class Commentaire
+    {
+        public int ID { get; set; }
+        public string Contenu { get; set; }
+        public virtual Annonce Annonce { get; set; }
+        public virtual Notation Notation { get; set; }
+        public virtual Utilisateur utilisateur { get; set; }
+    }
+}
